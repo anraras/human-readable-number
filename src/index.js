2b4397c8caa3772bdd255a593e1482857e29ebf4
anraras <oleg.fort27@gmail.com>
2022-07-06 16:25:58 +0300

    do test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
-  
-}
+// 
+
+module.exports = function toReadable (number)  {
+    let firstNUmbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tenNumbers = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let dozen = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let answer =''
+    
+    if (number === 0) {
+        answer = `${firstNUmbers[0]}`;
+    }
+    if (Math.trunc(number/100) != 0) {
+        answer = `${firstNUmbers[Math.trunc(number/100)]} hundred `;
+        number = number - Math.trunc(number/100)*100;
+    }
+    if ((Math.trunc(number/10) !=0) && (number > 19)) {
+        answer =answer + `${dozen[Math.trunc(number/10) -2]} `;
+        number = number - Math.trunc(number/10)*10;
+    }
+    if ((Math.trunc(number/10) !=0) && (number < 20)) {
+        answer =answer + `${tenNumbers[number%10]}`;
+        return answer.trim();
+    } else if (number > 0) { 
+        answer=answer + `${firstNUmbers[number]}`;
+
+    }
+
+
+
+
+    return answer.trim(); }
+
+
